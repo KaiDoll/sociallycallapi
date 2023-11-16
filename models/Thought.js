b1ab99097b20cf8bc05252b1225281a44c617af0
KaiDoll <132765530+KaiDoll@users.noreply.github.com>
2023-11-16 15:58:27 -0600

    Folders established and models file work in progress

---
+++ models/Thought.js
@@ -0,0 +1,65 @@
+const { Schema, model, Types } = require('mongoose');
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      timestamps: true
+    },
+  },
+  {
+    toJSON: {
+      getters: true, //check this 
+    },
+    id: false,
+  },
+);
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 280,
+      minlength: 1,
+    },
+    createdAt: {
+      type: Date, 
+      default: Date.now,
+      timestamps: true
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions;
+})
+const Thought = model('Thought', userSchema);
+
+module.exports = reactionSchema;
+module.exports = thoughtSchema;
