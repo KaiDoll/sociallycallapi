dc9ddacfd2ea51eef0dab569497279869ddc8c4e
KaiDoll <132765530+KaiDoll@users.noreply.github.com>
2023-11-17 14:28:02 -0600

    Successfully established a route in deleting a friend from the user

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
-const {createUser,getUsers,getSingleUser,updateUser,deleteUser} = require('../../controllers/userController')
+const {createUser,getUsers,getSingleUser,updateUser,deleteUser,addFriend,deleteFriend} = require('../../controllers/userController')
 
 router.route('/').post(createUser).get(getUsers);
 router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
 
 module.exports = router;
