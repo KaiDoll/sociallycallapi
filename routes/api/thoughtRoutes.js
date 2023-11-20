18a29e996f527ecc23cb65e53c690fd31c474f53
KaiDoll <132765530+KaiDoll@users.noreply.github.com>
2023-11-20 13:59:42 -0600

    All route complete!

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,7 +1,20 @@
-const router = require('express').Router();
-const {getThoughts,getSingleThought,newThought,updateThought, deleteThought,createReaction} = require('../../controllers/thoughtController');
+const router = require("express").Router();
+const {
+  getThoughts,
+  getSingleThought,
+  newThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  deleteReaction,
+} = require("../../controllers/thoughtController");
 
-router.route('/').get(getThoughts).post(newThought);
-router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
-router.route("/:thoughtId/reactions").post(createReaction)
+router.route("/").get(getThoughts).post(newThought);
+router
+  .route("/:thoughtId")
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+router.route("/:thoughtId/reactions").post(createReaction);
+router.route("/:thoughtId/reactions/:reactionId").delete(deleteReaction);
 module.exports = router;
